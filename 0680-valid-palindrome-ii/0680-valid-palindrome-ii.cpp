ffe665d8790bdc6a124a7186835f7eb0be575d84
SWARAAT CHATTERJEE <107751538+Swaraat@users.noreply.github.com>
2023-10-21 23:51:53 +0530

    Time: 54 ms (58.1%), Space: 24.3 MB (41.58%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s, int start, int end) {
+        while (start < end) {
+            if (s[start++] != s[end--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    bool validPalindrome(string s) {
+        int start = 0, end = s.length() - 1;
+        while (start < end) {
+            if (s[start] != s[end]) {
+                return isPalindrome(s, start + 1, end) || isPalindrome(s, start, end - 1);
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
