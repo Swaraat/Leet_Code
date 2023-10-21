243667953c79de72c95f93619bbed63e2aec8dca
SWARAAT CHATTERJEE <107751538+Swaraat@users.noreply.github.com>
2023-10-22 00:28:31 +0530

    Time: 13 ms (33.1%), Space: 16.1 MB (13.71%) - LeetHub

--- 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.cpp
+++ 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.cpp
@@ -4,7 +4,7 @@ public:
         int cnt=0,x=0;
         for(int i=1;i<nums.size();i++){
                 if(nums[i]<=nums[i-1]){
-                  x= abs(nums[i]-nums[i-1]);
+                  x= nums[i-1]-nums[i];
                     nums[i]=x+nums[i]+1;
                     cnt=cnt+x+1;
                 }
